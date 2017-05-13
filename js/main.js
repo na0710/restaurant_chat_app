07f203160d5ae1d62e8cf7a0f7a7326d043a9f0e
Nisha Agarwal <na07101997@gmail.com>
2017-05-14 02:15:54 +0530

    chnages in js file

--- js/main.js
+++ js/main.js
@@ -21,7 +21,8 @@
 		var msg = JSON.parse(e.data);
 		updateMessages(msg);
 	};
-
+    
+    var welcome = 'You can perform the following function : 1. Getting the Menu Card - menu 2. Know the cost of particular food - cost and then the name of the food 3. Know the food within a particular range - range and then the Maximum limit of the range 4. Contact Details - contact number 5. Order - order 6. Know the existing offers - offers 7. Know the time of the order - time of order 8. Know the total cost of order - total cost 9. Give the final order - final order' 
 
 	$('#join-chat').click(function(){
 		user = $('#user').val();
@@ -30,7 +31,8 @@
 
 		var msg = {
 			'user': user,
-			'text': user + ' entered the room',
+			'text': welcome,
+			
 			'time': moment().format('hh:mm a')
 		};
 
@@ -44,11 +46,37 @@
 	$('#send-msg').click(function(){
 		var text = $('#msg').val();
 		if (text == "menu"){text = "menu_card"; }
+        if (c == 4 ){
+		text = "food within this range";
+		c = 0
+	    }
+		if (c == 2 ){
+		text = "Your order have been saved";
+		c = 0;
+		var order_list = text;
+		c = 3;
+	    }
         if (c == 1 ){
 		if (text == "dosa"){text = "200"; c = 0;}
 	    }
 		if (text == "cost"){text = "ok_for which product";
 		c = 1; }
+		if (text == "contact number") {text = "Here are the contact details";}
+		if (text == "range") {text = "Enter your range";
+		c = 4;}
+		if (text == "order") {text = "What would you like to have ?"; 
+	       c = 2 ;}
+	    if (text == "offers") {text = "Display the offers available"; 
+	    }
+	    if ( c==3 ) {if (text == "time of order") {text = "30 minutes"; 
+	    } }
+	    if ( c==3 ) {if (text == "total cost") {text = "Your total cost is"; 
+	    } }
+	    if ( c==3 ) { if (text == "final order") {text = "Your order is finalized. Thankyou	";
+	     c = 0;} 
+	    }
+
+
 
 		var msg = {
 			'user': user,
@@ -82,4 +110,4 @@
 		
 	});
 
-})(); 	
+})();
